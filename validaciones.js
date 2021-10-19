8f1ef7a64076f17997f2c3f7d5b9cde017b094dd
IngJurado79 <42908182+IngJurado79@users.noreply.github.com>
2021-10-19 16:23:04 -0500

    Archivos De Código de los Sprint
    Archivos donde se puede verificar el trabajo del grupo uno sobre los Sprint propuesto en el modulo de desarrollo de software.

---
+++ validaciones.js
@@ -0,0 +1,19 @@
+function validar_nombreUsuario(String){
+    let input = document.getElementById("in_usuario");
+    let regxp = /^([a-zA-Z0-9_-]){3,9}$/;
+        if(input != regxp){
+        return(regxp.test(input.value));
+        }
+        
+    }
+    
+function validar_contrasena(String){
+    let input = document.getElementById("in_contrasena");
+    let regxp = /^([a-zA-Z0-9_-]){6,20}$/;
+        if(input != regxp){
+            return(regxp.test(input.value));
+        }
+        
+    }
+module.exports.validar_nombreUsuario = validar_nombreUsuario;
+module.exports.validar_contrasena = validar_contrasena;
